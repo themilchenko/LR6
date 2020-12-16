874c93a263d44d2c23a6c4429da9cd015fb37165
Ivan <70635146+themilchenko@users.noreply.github.com>
2020-12-16 17:00:11 +0300

    Update LR6.cpp

--- LR6.cpp
+++ LR6.cpp
@@ -15,18 +15,56 @@ struct address
 struct citizen
 {
     std::string snp; // surname name patronymic
-    address hmo; // housing maintenance office 
+    address hmo; // housing maintenance office
     std::string gender;
     int age;
 };
 
+std::ostream& operator<< (std::ofstream& output, const std::vector<citizen>& live)
+{
+    for(citizen i : live)
+    {
+        output << "Full name: " << i.snp << std::endl;
+        output << "Street living: " << i.hmo.street << std::endl;
+        output << "House living: " << i.hmo.house_number << std::endl;
+        output << "Flat living: " << i.hmo.flat_number << std::endl;
+        output << "Gender: " << i.gender << std::endl;
+        output << "Age: " << i.age << std::endl << std::endl;
+    }
+    return output;
+}
+
+std::ostream& operator<< (std::ostream& output, citizen& live)
+{
+    output << "Full name: " << live.snp << std::endl;
+    output << "Street living: " << live.hmo.street << std::endl;
+    output << "House living: " << live.hmo.house_number << std::endl;
+    output << "Flat living: " << live.hmo.flat_number << std::endl;
+    output << "Gender: " << live.gender << std::endl;
+    output << "Age: " << live.age << std::endl;
+}
+
+std::ifstream& operator>> (std::ifstream& input, const std::vector<citizen> live)
+{
+    for (auto i : live)
+    {
+        std::getline(input, i.snp);
+        std::getline(input, i.hmo.street);
+        input >> i.hmo.house_number;
+        input >> i.hmo.flat_number;
+        std::getline(input, i.gender);
+        input >> i.age;
+    }
+    return input;
+}
+
 int main()
 {
     // initialization objects
-    citizen first = { "Ivanon Ivan Ivanovich", "World's street", 50, 12, "male", 25 };
+    citizen first = { "Ivanov Ivan Ivanovich", "World's street", 50, 12, "male", 25 };
     citizen second = { "Pupkin Vasily Sergeevich", "Baker street", 62, 35, "male", 60 };
     citizen third = { "Vasileva Viktoria Pavlovna", "Tverskaya street", 31, 7, "female", 37 };
-    citizen fourth = { "Krasnova Maria Alecksandrova", "Nikolskaya street", 91, 80, "female", 17 };
+    citizen fourth = { "Krasnova Maria Alecksandrovna", "Nikolskaya street", 91, 80, "female", 17 };
 
     // adding this objects in vector
     std::vector <citizen> human(4);
@@ -36,51 +74,60 @@ int main()
     human[3] = fourth;
 
     // sorting names
-    for (int i = 0; i < 4; i++) 
+    for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4 - i - 1; j++)
             if (human[j].snp > human[j + 1].snp)
-        {
-            auto t = human[j];
-            human[j] = human[j + 1];
-            human[j + 1] = t;
-        }
+            {
+                auto t = human[j];
+                human[j] = human[j + 1];
+                human[j + 1] = t;
+            }
 
+    // finding number of people
     int k = 0;
     for (int i = 0; i < 4; i++)
         if ((human[i].age >= 18) && (human[i].age <= 27) && (human[i].gender == "male"))
-        {
             k++;
-            std::cout << "Full name: " << human[i].snp << std::endl;
-            std::cout << "Street living: " << human[i].hmo.street << std::endl;
-            std::cout << "House living: " << human[i].hmo.house_number << std::endl;
-            std::cout << "Flat living:  " << human[i].hmo.flat_number << std::endl;
-            std::cout << "Gender: " << human[i].gender << std::endl;
-            std::cout << "Age: " << human[i].age << std::endl;
-        }
-    std::cout << "Numer of people which age is after 18 and before 27 is " << k;
 
-    std::ofstream OutputFile("Text.txt", std::ios::out); // writing file
+    // writing file
+    std::ofstream OutputFile("Text.txt", std::ios::out);
     OutputFile << "People list: \n";
-    for (int i = 0; i < 4; i++) // writing people in sort list
-    {
-        OutputFile << "Full name: " << human[i].snp << std::endl;
-        OutputFile << "Street living: " << human[i].hmo.street << std::endl;
-        OutputFile << "House living: " << human[i].hmo.house_number << std::endl;
-        OutputFile << "Flat living:  " << human[i].hmo.flat_number << std::endl;
-        OutputFile << "Gender: " << human[i].gender << std::endl;
-        OutputFile << "Age: " << human[i].age << std::endl << std::endl;
-    }
-    OutputFile << std::endl << "Numer of people which age is after 18 and before 27 is " << k << std::endl;
+    OutputFile << human;
+    OutputFile << std::endl << "Number of people which age is after 18 and before 27 is " << k << std::endl;
     for (int i = 0; i < 4; i++)
         if ((human[i].age >= 18) && (human[i].age <= 27) && (human[i].gender == "male"))
-        {
-            OutputFile << "Full name: " << human[i].snp << std::endl;
-            OutputFile << "Street living: " << human[i].hmo.street << std::endl;
-            OutputFile << "House living: " << human[i].hmo.house_number << std::endl;
-            OutputFile << "Flat living:  " << human[i].hmo.flat_number << std::endl;
-            OutputFile << "Gender: " << human[i].gender << std::endl;
-            OutputFile << "Age: " << human[i].age << std::endl << std::endl;
-        }
+            OutputFile << human[i];
     OutputFile.close();
+
+    // read file
+    std::ifstream input("Text.txt", std::ios::in);
+    input >> human;
+
+
+    // reading binary
+    std::ifstream bin_input("Binary.bin", std::ios::binary);
+    for (auto& i : human)
+    {
+        bin_input.read(reinterpret_cast<char*>(&i.snp), sizeof(i.snp));
+        bin_input.read(reinterpret_cast<char*>(&i.hmo.street), sizeof(i.hmo.street));
+        bin_input.read(reinterpret_cast<char*>(&i.hmo.house_number), sizeof(i.hmo.house_number));
+        bin_input.read(reinterpret_cast<char*>(&i.hmo.flat_number), sizeof(i.hmo.flat_number));
+        bin_input.read(reinterpret_cast<char*>(&i.gender), sizeof(i.gender));
+        bin_input.read(reinterpret_cast<char*>(&i.age), sizeof(i.age));
+    }
+    bin_input.close();
+
+    // writing binary
+    std::ofstream bin_out("Binary.bin", std::ios::binary);
+    for (auto& i : human)
+    {
+        bin_out.write(reinterpret_cast<char*>(&i.snp), sizeof(i.snp));
+        bin_out.write(reinterpret_cast<char*>(&i.hmo.street), sizeof(i.hmo.street));
+        bin_out.write(reinterpret_cast<char*>(&i.hmo.house_number), sizeof(i.hmo.house_number));
+        bin_out.write(reinterpret_cast<char*>(&i.hmo.flat_number), sizeof(i.hmo.flat_number));
+        bin_out.write(reinterpret_cast<char*>(&i.gender), sizeof(i.gender));
+        bin_out.write(reinterpret_cast<char*>(&i.age), sizeof(i.age));
+    }
+
     return 0;
 }
